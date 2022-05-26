0009e76444bc647f59872926bdf79d8d21c9d5a0
Yuliia Skabitskaya <yulik777@i.ua>
2022-05-26 21:52:29 +0300

    first commit

---
+++ src/components/Counter.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import './Counter.css';
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
+  return (
+  <button onClick={handleClick}>{count}</button>
+  );
+};
+
+export default Counter;
